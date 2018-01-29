3ca0afe4174cd4069d7d935a0b67c7f7372338f6
mobapp07 <mobapp07@dt-c205-27.local>
2018-01-29 13:58:12 +0100

    All screens done

---
+++ DemoVistival/Day1ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Day1ViewController.swift
+//  DemoVistival
+//
+//  Created by mobapp07 on 29/01/2018.
+//  Copyright © 2018 mobapp07. All rights reserved.
+//
+
+import UIKit
+
+class Day1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
